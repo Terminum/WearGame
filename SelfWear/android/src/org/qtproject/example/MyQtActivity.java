7ce2b04f5e47955950fee7a2eda3a83994d13ce3
Kirill Yasny <k.yasny@skoltech.ru>
2023-12-27 18:55:10 +0300

    Refactor java class

--- SelfWear/android/src/org/qtproject/example/MyQtActivity.java
+++ SelfWear/android/src/org/qtproject/example/MyQtActivity.java
@@ -1,67 +1,22 @@
 package org.qtproject.qt5.android.bindings;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.WindowManager;
 import org.qtproject.qt.android.bindings.QtActivity;
 import org.qtproject.qt.android.QtNative;
 
-import android.content.Context;
-import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
-
 public class MyQtActivity extends QtActivity { // QtActivity
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-	System.out.println("On Created!!!!!!!!!!!!!!!!!!!!!!!!!");
-        // Отключение жеста "android:windowSwipeToDismiss"
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-    }
-    
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        // Переопределение жеста свайпа
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                // Обработка начала свайпа
-                System.out.println("Start Swipe Right!!!!!!!!!!!!!!!!!!!!!!!!!");
-                break;
-            case MotionEvent.ACTION_MOVE:
-                // Обработка движения при свайпе
-                System.out.println("Swipe Right!!!!!!!!!!!!!!!!!!!!!!!!!");
-                break;
-            case MotionEvent.ACTION_UP:
-                // Обработка завершения свайпа
-                System.out.println("End Swipe Right!!!!!!!!!!!!!!!!!!!!!!!!!");
-                break;
-        }
-        return super.onTouchEvent(event);
-    }
-    
         @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
         // Перехватываем событие свайпа слева направо
         if (event.getAction() == MotionEvent.ACTION_MOVE && event.getX() > 0) {
             // Возвращаем true, чтобы предотвратить передачу события выше по цепочке
-            System.out.println("Swipe Right Block!!!!!!!!!!!!!!!!!!!!!!!!!");
+            //System.out.println("Swipe Right Block!!!!!!!!!!!!!!!!!!!!!!!!!");
             return true;
         }
         return super.dispatchTouchEvent(event);
     }
 
-//    public void onSwipeRight() {
-	//    System.out.println("Swipe Right!!!!!!!!!!!!!!!!!!!!!!!!!");
-        // Whatever
-    //}
-    //setOnKeyListener { v, keyCode, event ->
-    //    if (keyCode == KeyEvent.KEYCODE_BACK) {
-    //        System.out.println("Hardware Back Clicked!")
-    //    }
-  //      true // If this is true then it will not send to onBackPressed() otherwise it will send
-//    }
 }
 
